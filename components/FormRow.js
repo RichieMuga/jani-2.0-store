2349ad3047c37bc8f2910cdfcf4964ea56e43ec7
Richie Mugambi <richiemugambi4@gmail.com>
2022-08-31 14:00:50 +0300

    halfway css navbar

--- components/FormRow.js
+++ components/FormRow.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import styles from '../styles/FormRow.module.css'
 
-const FormRow = () => {
+const FormRow = ({ type, name, value, handleChange, labelText, placeholder, form_label, form_input, form_row }) => {
     return (
-        <div>FormRow</div>
+        <div className={form_row}>
+            <label htmlFor={name} className={form_label}>
+                {labelText || name}
+            </label>
+
+            <input
+                type={type}
+                value={value}
+                name={name}
+                onChange={handleChange}
+                className={form_input}
+                placeholder={placeholder}
+            />
+        </div>
     )
 }
 
--- components/NavBar.js
+++ components/NavBar.js
@@ -1,8 +1,40 @@
 import styles from '../styles/NavBar.module.css'
+import FormRow from './FormRow';
+import Image from 'next/image'
+import logo from '../public/Logo(1).svg'
+import Link from 'next/link';
+import { IoMdSearch } from "react-icons/io";
+import { VscTriangleDown } from "react-icons/vsc";
+import { IoIosHelpCircleOutline } from "react-icons/io";
+import { BsCart } from "react-icons/bs";
+
 
 const NavBar = () => {
     return (
-        <div>NavBar</div>
+        <header className={styles.navBar}>
+            <div className={styles.navBarDiv}></div>
+            <div className={styles.navBarContent}>
+                <Link href={'/'}><Image src={logo} alt='Home' /></Link>
+                <section className={`dispFlex ${styles.search_ting}`}>
+
+                    <input type='text' className={styles.search_bar} />
+                    {/* // type={type}
+                    // value={value}
+                    // name={name}
+                    // onChange={handleChange}
+                    // className={form_input}
+                    // placeholder={placeholder} */}
+
+                    <button type="submit" className={styles.search_btn}>Search</button>
+                </section>
+                <p className='paragraph'>Accounts <VscTriangleDown size={12} /></p>
+                <p className='paragraph'><IoIosHelpCircleOutline size={25} />Help <VscTriangleDown /></p>
+                <section className='dispFlex'>
+                    <p className='paragraph'><BsCart size={25} /> Cart</p>
+                </section>
+
+            </div>
+        </header>
     )
 }
 
