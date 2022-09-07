bf51dc17076a9a53a5e166d955f60f4628d0d85d
Richie Mugambi <richiemugambi4@gmail.com>
2022-09-07 10:16:47 +0300

    added footer no functionality

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,84 @@
 import styles from '../styles/Footer.module.css'
+import { getToKnowUslinks, quickLinks, makeMoneyWithUs, letUsHelpYou } from '../utils/FooterLinks'
+import { BsTwitter, BsFacebook, BsInstagram } from "react-icons/bs";
+import Image from 'next/image'
+import smallLogo from '../public/small_Logo.svg'
+
+// BsTwitter
 
 const Footer = () => {
     return (
-        <div className={styles.footer}>Footer</div>
+        <div className={styles.footer}>
+            <button className={styles.btn_top} type="submit">Back to top</button>
+            {/* newsletter section */}
+            <section className={styles.newsLetter}>
+                <div className={styles.newsLetterTitle}>
+                    <h4>NewsLetter</h4>
+                </div>
+                <div className={styles.textNsubscribe}>
+                    <p>Subscribe to keep up with our great deals.</p>
+                    <section className={styles.btnNinput}>
+                        <input type="text" className={styles.nws_form_input} placeholder='Your Email' />
+                        <button className={styles.nws_btn} type="submit">Subscribe</button>
+                    </section>
+                </div>
+            </section>
+            {/* links section */}
+            <section className={styles.links_heading}>
+                <div className={styles.links_individual}>
+                    <h4>Get to know us</h4>
+                    {getToKnowUslinks.map((individualLinks) => {
+                        return (
+                            <>
+                                <p>{individualLinks.text}</p>
+                            </>
+                        )
+                    })}
+                </div>
+                <div className={styles.links_individual}>
+                    <h4>Quick Links</h4>
+                    {quickLinks.map((individualLinks) => {
+                        return (
+                            <>
+                                <p>{individualLinks.text}</p>
+                            </>
+                        )
+                    })}
+                </div>
+                <div className={styles.links_individual}>
+                    <h4>Make money with us</h4>
+                    {makeMoneyWithUs.map((individualLinks) => {
+                        return (
+                            <>
+                                <p>{individualLinks.text}</p>
+                            </>
+                        )
+                    })}
+                </div>
+                <div className={styles.links_individual}>
+                    <h4>Let us help you</h4>
+                    {letUsHelpYou.map((individualLinks) => {
+                        return (
+                            <>
+                                <p>{individualLinks.text}</p>
+                            </>
+                        )
+                    })}
+                </div>
+            </section>
+            {/* socials section */}
+            <section className="flex_kidogo">
+                <BsTwitter size={22} />
+                <BsFacebook size={22} />
+                <BsInstagram size={22} />
+            </section>
+            {/* final footer section */}
+            <section className='flex_kidogo'>
+                <Image src={smallLogo}></Image>
+                <p className='smallP'>	©  2022 jani.com</p>
+            </section>
+
+        </div>
     )
 }
 
---
+++ utils/FooterLinks.js
@@ -0,0 +1,27 @@
+export const getToKnowUslinks = [
+    { id: 1, text: 'About', path: '/about' },
+    { id: 2, text: 'Blog', path: '/blog' },
+    { id: 3, text: 'Investor Relations', path: '/investorPage' },
+    { id: 4, text: 'News', path: '/news' },
+]
+
+export const quickLinks = [
+    { id: 1, text: 'Daily deals', path: '/' },
+    { id: 2, text: 'Policy', path: '/policy' },
+    { id: 3, text: 'Privacy policy', path: '/privacyPolicy' },
+    { id: 4, text: 'Refund Policy', path: '/refundPolicy' },
+]
+
+export const makeMoneyWithUs = [
+    { id: 1, text: 'Sell', path: '/sellWithUs' },
+    { id: 2, text: 'How to sell', path: '/sellHelp' },
+    { id: 3, text: 'Sell Electronics', path: '/sellWithUs' },
+    { id: 4, text: 'Sell Beauty Products', path: '/sellWithUs' },
+]
+
+export const letUsHelpYou = [
+    { id: 1, text: 'Help', path: '/help' },
+    { id: 2, text: 'Faqs', path: '/faqs' },
+    { id: 3, text: 'Customer Care', path: '/customerCare' },
+]
+
